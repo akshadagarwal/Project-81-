667d3e27d36103d1212cdbe649787b439b4cc77d
akshadagarwal <96433162+akshadagarwal@users.noreply.github.com>
2021-12-20 20:47:32 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,61 @@
+// AKSHAD AGARWAL IMPORTANT PROJECT
+
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=2;
+ctx.arc(290, 230, 40, 0, 2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown", my_mousedown);
+
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=2;
+ctx.arc(240, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown", my_mousedown);
+
+
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=2;
+ctx.arc(390, 230, 40, 0, 2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown", my_mousedown);
+
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=2;
+ctx.arc(340, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=2;
+ctx.arc(440, 200, 40, 0, 2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown", my_mousedown);
+
+
+
+function my_mousedown(e)
+{
+    color = document.getElementById("color").value;
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(mouse_x, mouse_y, 20, 0, 2*Math.PI);
+ctx.stroke();
+}
